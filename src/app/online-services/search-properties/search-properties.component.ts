6aaff71efc685c92d11a5cf3fb008c1403e965cd
hugoage <hgarcia@ontica.org>
2018-04-30 18:44:49 -0500

    Add Search properties component

---
+++ src/app/online-services/search-properties/search-properties.component.ts
@@ -0,0 +1,31 @@
+import { Component, EventEmitter, HostBinding,  Input, Output, OnInit } from '@angular/core';
+import { DistrictItemId } from '../data-types/types';
+
+@Component({
+  selector: 'app-search-properties',
+  templateUrl: './search-properties.component.html',
+  styleUrls: ['./search-properties.component.css']
+})  
+export class SearchPropertiesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  public setMunicipalityInitialValues(selectedValue: string): void {
+    
+  }
+
+
+    // Enumeration re-declaration for use in html code.
+    public DistrictItemId = DistrictItemId;
+    public selectedDocumentItemType =  DistrictItemId.empty;
+  
+    public setDocumentItemInitialValues(selectedValue: string): void {
+      this.selectedDocumentItemType = Number(selectedValue);
+      //this.certificateRequest.certificateType = Number(selectedValue);
+      //this.setDocumentItemTypePattern();
+    }
+
+}
