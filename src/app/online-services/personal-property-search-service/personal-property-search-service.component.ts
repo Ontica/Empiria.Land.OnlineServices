9b4cd9cb6c4ab58689806b6476b3786264abaf6a
hugoage <hgarcia@ontica.org>
2018-05-09 22:06:09 -0500

    Personal propoerty search service (beta version)

--- src/app/online-services/personal-property-search-service/personal-property-search-service.component.ts
+++ src/app/online-services/personal-property-search-service/personal-property-search-service.component.ts
@@ -1,31 +1,40 @@
-import { Component, EventEmitter, HostBinding,  Input, Output, OnInit } from '@angular/core';
-import { DistrictItemId } from '../data-types/types';
+import { Component, EventEmitter, HostBinding,  Input, Output } from '@angular/core';
+import { Router } from '@angular/router';
+import { Validate } from '../services/validate';
 
 @Component({
   selector: 'app-personal-property-search-service',
   templateUrl: './personal-property-search-service.component.html',
   styleUrls: ['./personal-property-search-service.component.css']
 })  
-export class PersonalPropertySearchServiceComponent implements OnInit {
+export class PersonalPropertySearchServiceComponent  {
+  public selectedMunicipalityValue = 0;
+  public names:string = '';
+  public personalPropertyNames:string;
+  public data:string;
 
-  constructor() { }
+  constructor( private _router: Router) { }
 
-  ngOnInit() {
-  }
 
   public setMunicipalityInitialValues(selectedValue: string): void {
+    this.selectedMunicipalityValue = Number(selectedValue);
+    this.names = '';
+    this.data = '';
     
   }
 
-
-    // Enumeration re-declaration for use in html code.
-    public DistrictItemId = DistrictItemId;
-    public selectedDocumentItemType =  DistrictItemId.empty;
-  
-    public setDocumentItemInitialValues(selectedValue: string): void {
-      this.selectedDocumentItemType = Number(selectedValue);
-      //this.certificateRequest.certificateType = Number(selectedValue);
-      //this.setDocumentItemTypePattern();
+  public validatePersonalPropertySearchData(names:string , data:string): void {
+    if( this.selectedMunicipalityValue!=0 && names!='' && names!= ' ' && data!='' && data!= ' ' ){
+    this._router.navigate(['/PaymentOrder/3/'+names+'/'+data+'/'+this.selectedMunicipalityValue]);
+    }else
+    {
+      alert('Necesito los datos solicitados para continuar...');
     }
+  }
+
+
+ 
+  
+   
 
 }
