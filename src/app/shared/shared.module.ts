354bf37eeef76c5f987b8a9369bfb401d8a301bd
jmcota <jmcota@ontica.org>
2018-06-11 16:31:50 -0400

    Remove unused controls (chips, rag, former spinner, safe-html)

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,50 +1,62 @@
 /**
  * @license
- * Copyright (c) 2017 La Vía Óntica SC, Ontica LLC and contributors. All rights reserved.
+ * Copyright (c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved.
  *
  * See LICENSE.txt in the project root for complete license information.
- *
  */
 
 import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+
+import { CoreModule } from '@app/core/core.module';
+
 import { MainLayoutComponent } from './main-layout/main-layout.component';
 
 import { NavbarComponent } from './navbar/navbar.component';
 import { NoContentComponent } from './no-content/no-content.component';
 
-import { SafeHtmlPipe } from './pipes/safe-html.pipe';
-
 import { AutocompleteControl } from './controls/autocomplete-control';
-import { Chips } from './controls/chips';
-import { RAGControl } from './controls/rag.control';
 import { CalendarControl } from './controls/calendar-control';
 import { SelectControl } from './controls/select-control';
 
-import { ProjectSelectorControl } from './controls/project-selector-control';
-
 import { NavBarService } from './navbar/navbar.service';
-import { ProjectSelectorService } from './controls/project-selector.service';
 
-import { SpinnerComponent } from './spinner/spinner.component';
-import { SpinnerService } from './spinner/spinner.service';
-import { ModalWindowComponent } from './windows/modal-window/modal-window';
-
-import { MessageBox } from './windows/message-box/message-box.component';
 
 /**
  * Do not specify providers for modules that might be imported by a lazy loaded module.
  */
 
 @NgModule({
-  imports: [RouterModule, CommonModule, FormsModule],
-  declarations: [MainLayoutComponent, NavbarComponent, NoContentComponent, SafeHtmlPipe,
-                 AutocompleteControl, Chips, RAGControl, CalendarControl, SelectControl,
-                ProjectSelectorControl,SpinnerComponent, ModalWindowComponent,MessageBox],
-  exports: [MainLayoutComponent, NoContentComponent, SafeHtmlPipe, AutocompleteControl, Chips,
-            RAGControl, CalendarControl, SelectControl,  ModalWindowComponent,MessageBox],
-  providers:[NavBarService, ProjectSelectorService, SpinnerService]
+
+  imports: [
+    CommonModule,
+    RouterModule,
+    FormsModule,
+    CoreModule
+  ],
+
+  declarations: [
+    AutocompleteControl,
+    CalendarControl,
+    MainLayoutComponent,
+    NavbarComponent,
+    NoContentComponent,
+    SelectControl
+  ],
+
+  exports: [
+    AutocompleteControl,
+    CalendarControl,
+    MainLayoutComponent,
+    NoContentComponent,
+    SelectControl
+  ],
+
+  providers:[
+    NavBarService
+  ]
+
 })
 export class SharedModule { }
