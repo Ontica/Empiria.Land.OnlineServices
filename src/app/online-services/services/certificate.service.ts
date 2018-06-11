f42cd9107e43c9426a6341ec901e89beede8e7da
jmcota <jmcota@ontica.org>
2018-06-11 16:27:54 -0400

    Migrate RxJs code from version 5.5 to 6.2

--- src/app/online-services/services/certificate.service.ts
+++ src/app/online-services/services/certificate.service.ts
@@ -1,36 +1,39 @@
 /**
  * @license
- * Copyright (c) 2018 La Vía Óntica SC, Ontica LLC and contributors. All rights reserved.
+ * Copyright (c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved.
  *
  * See LICENSE.txt in the project root for complete license information.
- *
  */
 
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 import { CoreService } from '../../core/core.service';
 
-
 import { CertificateRequest } from '../data-types/CertificateRequest';
 
-export enum CertificateServiceErr {
-  GET_PROPERTY_ERR = 'GET_PROPERTY_ERR] No pude leer el folio de predio.'
+
+enum Errors {
+
+  GET_PROPERTY_ERR =
+  '[GET_PROPERTY_ERR] No pude leer el folio de predio.'
+
 }
+
 
 @Injectable()
 export class CertificateService {
 
-  constructor(private core: CoreService) {
-
-  }
+  constructor(private core: CoreService) { }
 
   public existsProperty(propertyUID: string): Observable<CertificateRequest[]> {
     const path = `v1/online-services/resources/${propertyUID}`;
 
     return this.core.http.get<CertificateRequest[]>(path)
-      .catch((e) => this.core.http.showAndReturn(e, CertificateServiceErr.GET_PROPERTY_ERR, null))
-
+      .pipe(
+        catchError((e) => this.core.http.showAndThrow(e, Errors.GET_PROPERTY_ERR))
+      );
   }
 
 }
--- src/app/online-services/services/copy.service.ts
+++ src/app/online-services/services/copy.service.ts
@@ -1,55 +1,50 @@
 /**
  * @license
- * Copyright (c) 2018 La Vía Óntica SC, Ontica LLC and contributors. All rights reserved.
+ * Copyright (c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved.
  *
  * See LICENSE.txt in the project root for complete license information.
- *
  */
 
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 import { CoreService } from '../../core/core.service';
 
-
 import { DocumentCopyRequest } from '../data-types/DocumentCopyRequest';
 
-export enum CopyServiceErr {
-  GET_PENDING_ERR =
-  '[GET_PENDING_ERR] No pude leer el documento o tramite.',
-  GET_REFUSED_ERR =
-  '[GET_REFUSED_ERR] Ocurrió un problema al leer los documentos regresados.',
-  POST_REFUSED_ERR =
-  '[POST_REFUSED_ERR] Ocurrió un problema al guardar los documentos rechazados.',
-  POST_REVOKE_ERR =
-  '[POST_REVOKE_ERR] Ocurrió un problema al enviar los documentos revocados.',
-  POST_UNREFUSE_ERR =
-  '[POST_UNREFUSED_ERR] Ocurrió un problema al regresar  los documentos rechazados.',
+
+export enum Errors {
+  GET_DOCUMENT =
+  '[GET_DOCUMENT] No pude leer el documento.',
+
+  GET_FILING =
+  '[GET_FILING] No pude leer el trámite.',
 }
 
 
 @Injectable()
 export class CopyService {
-  constructor(private core: CoreService) { }
 
+  constructor(private core: CoreService) { }
 
   public existsDocument(documentUID: string): Observable<DocumentCopyRequest[]> {
     const path = `v1/online-services/documents/${documentUID}`;
 
-    return this.core.http
-      .get<DocumentCopyRequest[]>(path)
-      .catch((e) => this.core.http.showAndReturn(e, CopyServiceErr.GET_PENDING_ERR, null))
+    return this.core.http.get<DocumentCopyRequest[]>(path)
+      .pipe(
+        catchError((e) => this.core.http.showAndThrow(e, Errors.GET_DOCUMENT))
+      );
   }
 
 
   public existsFiling(filingUID: string): Observable<DocumentCopyRequest[]> {
     const path = `v1/online-services/transactions/${filingUID}`;
 
-    return this.core.http
-      .get<DocumentCopyRequest[]>(path)
-      .catch((e) => this.core.http.showAndReturn(e, CopyServiceErr.GET_PENDING_ERR, null))
+    return this.core.http.get<DocumentCopyRequest[]>(path)
+      .pipe(
+        catchError((e) => this.core.http.showAndThrow(e, Errors.GET_FILING))
+      );
   }
 
-
 }
-
